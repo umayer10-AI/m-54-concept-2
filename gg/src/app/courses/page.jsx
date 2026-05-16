5e008ad24ec41fa44a2deb48d1c41cb92c784e73
umayer10-AI <mdumayer10@gmail.com>
2026-05-16 19:35:47 +0600

    add

---
+++ gg/src/app/courses/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <div className='text-center space-y-5 my-10'>
+                <h2 className='text-5xl font-extrabold'>Explore Our <span className='bg-linear-to-r from-blue-500 to-blue-800 bg-clip-text w-fit text-transparent'>Premium</span> Courses</h2>
+                <h2 className=' text-gray-500 text-xl w-2xl mx-auto'>Find the perfect course to advance your career. Learn from the best experts in the field.</h2>
+            </div>
+        </div>
+    );
+};
+
+export default page;
