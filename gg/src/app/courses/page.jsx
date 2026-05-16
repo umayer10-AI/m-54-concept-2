1c4e5e10f4f2a8258b993e0ca9d0ee53e6240640
umayer10-AI <mdumayer10@gmail.com>
2026-05-16 19:38:27 +0600

    add

--- gg/src/app/courses/page.jsx
+++ gg/src/app/courses/page.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PiBookOpen } from 'react-icons/pi';
 
 const page = () => {
     return (
@@ -6,6 +7,10 @@ const page = () => {
             <div className='text-center space-y-5 my-10'>
                 <h2 className='text-5xl font-extrabold'>Explore Our <span className='bg-linear-to-r from-blue-500 to-blue-800 bg-clip-text w-fit text-transparent'>Premium</span> Courses</h2>
                 <h2 className=' text-gray-500 text-xl w-2xl mx-auto'>Find the perfect course to advance your career. Learn from the best experts in the field.</h2>
+            </div>
+            <div>
+                <h2 className='text-2xl font-bold flex items-center gap-2'><span className='text-blue-500 text-3xl'><PiBookOpen /></span>All Courses</h2>
+                
             </div>
         </div>
     );
