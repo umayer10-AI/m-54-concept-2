02047336a180a66f37900c97a87e07ce669fa861
umayer10-AI <mdumayer10@gmail.com>
2026-05-16 18:03:08 +0600

    add

---
+++ gg/src/component/Nav1.jsx
@@ -0,0 +1,48 @@
+"use client"
+import { Button } from '@heroui/react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+const Nav1 = () => {
+
+    const p = usePathname()
+
+    const links = [
+        {
+            name: "Home",
+            path: "/"
+        },
+        {
+            name: "courses",
+            path: "/courses"
+        },
+        {
+            name: "Add Course",
+            path: "/add-course"
+        },
+        {
+            name: "Dashboard",
+            path: "/dashboard"
+        },
+    ]
+
+    return (
+        <div>
+            {
+                links.map(v => {
+                    <Link key={v.name} href={v.path} className='text-sm font-semibold'>
+                        {
+                            p===v.path? <Button size='sm' className={'bg-linear-to-r from-cyan-500 to-blue-600'}>
+                                {v.name}
+                            </Button>
+                            : <button>{v.name}</button>
+                        }
+                    </Link>
+                })
+            }
+        </div>
+    );
+};
+
+export default Nav1;
--- gg/src/component/Navbar.jsx
+++ gg/src/component/Navbar.jsx
@@ -1,13 +1,15 @@
 import Image from 'next/image';
 import React from 'react';
 import { FaBookOpen } from 'react-icons/fa';
+import Nav1 from './Nav1';
 
 const Navbar = () => {
     return (
         <div>
             <div>
-                <h2 className='flex items-center gap-1'><h2 className='text-3xl'><FaBookOpen /></h2><span className='text-2xl font-bold'>Mentora</span></h2>
+                <h2 className='flex items-center gap-1'><h2 className='text-4xl text-blue-500'><FaBookOpen /></h2><span className='text-2xl font-extrabold'>Mentora</span></h2>
             </div>
+            <Nav1></Nav1>
         </div>
     );
 };
