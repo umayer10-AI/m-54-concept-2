c59c2e6ce83373e1ade34b79a27d24b08b94f43a
umayer10-AI <mdumayer10@gmail.com>
2026-05-16 22:09:43 +0600

    add

--- gg/src/component/Nav2.jsx
+++ gg/src/component/Nav2.jsx
@@ -1,4 +1,5 @@
 "use client"
+import { authClient } from '@/lib/auth-client';
 import { Button } from '@heroui/react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -7,20 +8,30 @@ import React from 'react';
 const Nav2 = () => {
 
     const p = usePathname()
+    const { data: session } = authClient.useSession()
+    const user = session?.user
+    console.log(session)
 
     return (
         <div>
-            <div className='flex items-center gap-5'>
-                <Link href={'/login'} className='font-semibold'>
-                    {
-                        p==='/login' ? <Button size='sm' className={'bg-linear-to-r from-cyan-500 to-blue-600'}>
-                            Login
-                        </Button>
-                        : <button>Login</button>
-                    }
-                </Link>
-                <Link href={'/signup'}><Button size='sm' className=' bg-blue-500'>Join Free</Button></Link>
-            </div>
+            {
+                user? <div>
+                        <Button onClick={async() => await authClient.signOut()} variant='danger-soft'>Log Out</Button>
+                      </div>
+                    : <div className='flex items-center gap-5'>
+                        <Link href={'/login'} className='font-semibold'>
+                            {
+                                p==='/login' ? <Button size='sm' className={'bg-linear-to-r from-cyan-500 to-blue-600'}>
+                                    Login
+                                </Button>
+                                : <button>Login</button>
+                            }
+                        </Link>
+                        <Link href={'/signup'}><Button size='sm' className=' bg-blue-500'>Join Free</Button></Link>
+                    </div>
+            }
+            
+            
         </div>
     );
 };
