6b3a69079ebc25c66bed30cd62f0269346eeefda
umayer10-AI <mdumayer10@gmail.com>
2026-05-17 08:24:20 +0600

    add

--- gg/src/proxy.js
+++ gg/src/proxy.js
@@ -1,9 +1,19 @@
 import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+import { headers } from "next/headers";
 
-export function proxy(request) {
-  return NextResponse.redirect(new URL('/home', request.url))
+export async function proxy(request) {
+
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    if(session){
+        return NextResponse.next()
+    }
+
+    return NextResponse.redirect(new URL('/login', request.url))
 }
 
 export const config = {
-  matcher: ['/dashboard'],
+  matcher: ['/dashboard','/courses/:path+','/add-course'],
 }
